075a3e9fce96fd733313c44f6796392e007c1f10
Eclipse7PlusPlus <shellytoptop2298@gmail.com>
2025-03-15 20:29:07 +0300

    Initial commit

---
+++ CMakeFiles/Our_project_lab2.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Our_project_lab2.dir/main.cpp.o"
+  "CMakeFiles/Our_project_lab2.dir/main.cpp.o.d"
+  "Our_project_lab2"
+  "Our_project_lab2.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Our_project_lab2.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
