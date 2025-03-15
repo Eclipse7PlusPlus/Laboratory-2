2e84815c0d3ef0ca796e9d16cc474ab049f3b1e8
Eclipse7PlusPlus <shellytoptop2298@gmail.com>
2025-03-16 00:50:20 +0300

    Function AverageCalculate was added

--- main.cpp
+++ main.cpp
@@ -2,8 +2,9 @@
 #include <cstdint>
 
 void FillArray(int32_t arr[], int32_t size);
-int32_t FindMaxElementIndex(int32_t arr[], int32_t size);
-int32_t FindMinElementIndex(int32_t arr[], int32_t size);
+int32_t MaxElementIndex(int32_t arr[], int32_t size);
+int32_t MinElementIndex(int32_t arr[], int32_t size);
+double CalculateAverage(int32_t arr[], int32_t size); 
 
 int main() {
     const int32_t size{10};
@@ -11,23 +12,25 @@ int main() {
 
     FillArray(arr, size);
 
-    int32_t maxIndex = FindMaxElementIndex(arr, size);
-    int32_t minIndex = FindMinElementIndex(arr, size);
+    int32_t maxIndex = MaxElementIndex(arr, size);
+    int32_t minIndex = MinElementIndex(arr, size);
+    double average = CalculateAverage(arr,size); 
 
     std::cout << "Максимальный элемент: " << arr[maxIndex] << " на позиции: " << maxIndex << std::endl;        
     std::cout << "Минимальный элемент: " << arr[minIndex] << " на позиции: " << minIndex << std::endl;
-             
+    std::cout <<"Среднее арифметическое максимального и минимального элементов: " << average << std::endl;         
     return 0;
 }
 
 void FillArray(int32_t arr[], int32_t size) {
     std::cout << "Введите " << size << " элементов массива:" << std::endl;
-    for (int32_t i = 0; i < size; ++i) {
+    for (int32_t i = 0; i < size; ++i) 
+    {
         std::cin >> arr[i];
     }
 }
 
-int32_t FindMaxElementIndex(int32_t arr[], int32_t size) {
+int32_t MaxElementIndex(int32_t arr[], int32_t size) {
     int32_t maxIndex = 0;
     for (int32_t i = 1; i < size; ++i) {
         if (arr[i] > arr[maxIndex]) {
@@ -37,7 +40,7 @@ int32_t FindMaxElementIndex(int32_t arr[], int32_t size) {
     return maxIndex;
 }
 
-int32_t FindMinElementIndex(int32_t arr[], int32_t size) {
+int32_t MinElementIndex(int32_t arr[], int32_t size) {
     int32_t minIndex = 0;
     for (int32_t i = 1; i < size; ++i) {
         if (arr[i] < arr[minIndex]) {
@@ -45,4 +48,20 @@ int32_t FindMinElementIndex(int32_t arr[], int32_t size) {
         }
     }
     return minIndex;
+}
+double CalculateAverage(const int32_t arr[], int32_t size) {
+    int32_t maxElement = arr[0];
+    int32_t minElement = arr[0];
+    for (int32_t i = 1; i < size; ++i) {
+        if (arr[i] > maxElement) {
+            maxElement = arr[i];
+        }
+    }
+    
+    for (int32_t i = 1; i < size; ++i) {
+        if (arr[i] < minElement) {
+            minElement = arr[i];
+        }
+    }
+    return (maxElement+minElement)/2;
 }
