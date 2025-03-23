3029e783f855a4163c812bbc13ee8c088dd0823c
Eclipse7PlusPlus <shellytoptop2298@gmail.com>
2025-03-23 12:31:24 +0300

    Update functions.cpp

--- functions.cpp
+++ functions.cpp
@@ -4,8 +4,7 @@
 
 template <typename T>
 
-void InputArraySize(int32_t& arraySize)
-{
+void InputArraySize(int32_t& arraySize) {
     std::cout << "Input size of array: ";
     std::cin >> arraySize;
 }
@@ -22,31 +21,33 @@ void InputArray(T* array, int32_t size) {
     }
 }
 
+template <typename T>
 void PrintArray(const T* array, int32_t size) {
     for (int32_t i = 0; i < size; ++i) {
         std::cout << array[i] << ' ';
     }
     std::cout << '\n';
-} 
+}
 
 template <typename T>
 int32_t FindElement(const T* array, int32_t size, T target) {
     int32_t position = -1;
     for (int32_t i = 0; i < size; ++i) {
         if (array[i] == target) {
-            position = i + 1; 
+            position = i + 1; // Возвращаем позицию, начиная с 1
         }
     }
     return position;
 }
+
 int32_t MaxElementIndex(const int32_t* array, int32_t size) {
     int32_t maxIndex = 0;
     for (int32_t i = 1; i < size; ++i) {
         if (array[i] > array[maxIndex]) {
-            maxIndex = i+1;
+            maxIndex = i;
         }
     }
-    return maxIndex;
+    return maxIndex + 1; // Возвращаем позицию, начиная с 1
 }
 
 int32_t MinElementIndex(const int32_t* array, int32_t size) {
@@ -56,18 +57,18 @@ int32_t MinElementIndex(const int32_t* array, int32_t size) {
             minIndex = i;
         }
     }
-    return minIndex+1;
+    return minIndex + 1; // Возвращаем позицию, начиная с 1
 }
 
 template <typename T>
 double CalculateAverage(const T* array, int32_t size, int32_t maxIndex, int32_t minIndex) {
-    maxIndex -= 1;
+    maxIndex -= 1; // Корректируем индексы, так как функции возвращают позиции, начиная с 1
     minIndex -= 1;
 
     if (maxIndex == minIndex || abs(maxIndex - minIndex) == 1) {
         return -1; // Нет элементов между min и max
     }
-    
+
     int32_t start = (minIndex < maxIndex) ? minIndex : maxIndex;
     int32_t end = (minIndex < maxIndex) ? maxIndex : minIndex;
 
@@ -86,17 +87,26 @@ double CalculateAverage(const T* array, int32_t size, int32_t maxIndex, int32_t 
 }
 
 int32_t isPrime(int32_t num) {
-    if (num <= 1) return 0; 
-    if (num <= 3) return 1; 
-    if (num % 2 == 0 || num % 3 == 0) return 0; 
+    if (num <= 1) return 0;
+    if (num <= 3) return 1;
+    if (num % 2 == 0 || num % 3 == 0) return 0;
 
-   
     for (int32_t i = 5; i * i <= num; i += 6) {
         if (num % i == 0 || num % (i + 2) == 0) {
             return 0;
         }
     }
     return 1;
+}
+
+void PrimeNumbers(const int32_t* array, int32_t size) {
+    std::cout << "Prime numbers: ";
+    for (int32_t i = 0; i < size; ++i) {
+        if (isPrime(array[i])) {
+            std::cout << array[i] << ' ';
+        }
+    }
+    std::cout << '\n';
 }
 
 int32_t SumOfPrimes(const int32_t* array, int32_t size) {
@@ -124,7 +134,6 @@ int32_t DeleteNegativeNumbersFromArray(int32_t* array, int32_t size) {
             array[newSize++] = array[i];
         }
     }
-  
     for (int32_t i = newSize; i < size; ++i) {
         array[i] = 0;
     }
@@ -150,10 +159,10 @@ int32_t CountElements(const T* array, int32_t size) {
 }
 
 template <typename T>
-void bubbleSort(T* array, int32_t size) {
+void bubbleSort(T* array, int32_t size, bool ascending = true) {
     for (int32_t i = 0; i < size - 1; ++i) {
         for (int32_t j = 0; j < size - i - 1; ++j) {
-            if (array[j] > array[j + 1]) {
+            if ((ascending && array[j] > array[j + 1]) || (!ascending && array[j] < array[j + 1])) {
                 T temp = array[j];
                 array[j] = array[j + 1];
                 array[j + 1] = temp;
