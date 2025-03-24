0e67631e42a3480fa4c81492972ae553f91600f4
Eclipse7PlusPlus <shellytoptop2298@gmail.com>
2025-03-24 23:36:06 +0300

    Update header.h

--- header.h
+++ header.h
@@ -11,26 +11,26 @@ template <typename T>
 void InputArray(T* array, int32_t size);
 
 template <typename T>
-void PrintArray(const T* array, int32_t size);
+void PrintArray(T* array, int32_t size);
 
 template <typename T>
-int32_t FindElement(const T* array, int32_t size, T target);
+int32_t FindElement(T* array, int32_t size, T target);
 
-int32_t MaxElementIndex(const int32_t* array, int32_t size);
-int32_t MinElementIndex(const int32_t* array, int32_t size);
+int32_t MaxElementIndex(int32_t* array, int32_t size);
+int32_t MinElementIndex(int32_t* array, int32_t size);
 
 template <typename T>
-double CalculateAverage(const T* array, int32_t size, int32_t maxIndex, int32_t minIndex);
+double CalculateAverage(T* array, int32_t size, int32_t maxIndex, int32_t minIndex);
 
 int32_t isPrime(int32_t num);
-void PrimeNumbers(const int32_t* array, int32_t size);
-int32_t SumOfPrimes(const int32_t* array, int32_t size);
+void PrimeNumbers(int32_t* array, int32_t size);
+int32_t SumOfPrimes(int32_t* array, int32_t size);
 
 void SwapElementsOfArray(int32_t* array, int32_t size);
 int32_t DeleteNegativeNumbersFromArray(int32_t* array, int32_t size);
 
 template <typename T>
-int32_t CountElements(const T* array, int32_t size);
+int32_t CountElements(T* array, int32_t size);
 
 template <typename T>
 void bubbleSort(T* array, int32_t size, bool ascending = true);
