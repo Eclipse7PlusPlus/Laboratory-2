5c27ecba662aa47bd257ab6c986fe88b8eefc360
Eclipse7PlusPlus <shellytoptop2298@gmail.com>
2025-03-25 09:46:32 +0300

    Update functions.cpp

--- functions.cpp
+++ functions.cpp
@@ -71,11 +71,10 @@ void ReverseArray(int32_t* array, int32_t size) {
 }
 
 int32_t DeleteNegativeNumbersFromArray(int32_t* array, int32_t size) {
-    int32_t newSize = 0;
     for (int32_t i = 0; i < size; ++i) {
-        if (array[i] >= 0) {
-            array[newSize++] = array[i];
+        if (array[i] < 0) {
+            array[i] = 0;
         }
     }
-    return newSize;
+    return size; 
 }
